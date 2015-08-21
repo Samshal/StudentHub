f8661f27eceb922fd2df77b2ec8d2771a69ae91b
Samuel Adeshina <samueladeshina73@gmail.com>
2015-08-21 13:37:11 +0100

    Links, Uniform Resource Gens, Routers For use by the Controller

---
+++ __Alloc/__Alloc.php
@@ -0,0 +1,10 @@
+<?php
+	DEFINE("__DATABASE_DEPENDENCY", "../__Database");
+	DEFINE("__EXTERNAL_FOLDER", "../__External");
+	DEFINE("__TEMPFILES_FOLDER", "../__TempFiles");
+	DEFINE("__CONTROLLER_FOLDER", "../Controller");
+	DEFINE("__MODEL_FOLDER", "../Model");
+	DEFINE("__VIEW_FOLDER", "");
+	DEFINE("__MISC_FOLDER", "../Misc");
+	//error_reporting(0);
+?>
