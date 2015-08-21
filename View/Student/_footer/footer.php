a83248066da19d6dde36c4cdeff75447530e0e0f
Samuel Adeshina <samueladeshina73@gmail.com>
2015-08-21 14:34:49 +0100

    Student Page Parts

---
+++ View/Student/_footer/footer.php
@@ -0,0 +1,13 @@
+<script src="<?php echo $_JQUERY; ?>"></script>
+<script src="<?php echo $_JQUERYUIJS; ?>"></script>
+<script src="<?php echo $_BOOTSTRAPJS; ?>"></script>
+<script src="<?php echo $_MORRISJS; ?>"></script>
+<script src="<?php echo $_SPARKLINE; ?>"></script>
+<script src="<?php echo $_JVECTORMAPJS; ?>"></script>
+<script src="<?php echo $_JVECTORMAPWORLDJS; ?>"></script>
+<script src="<?php echo $_JQUERYKNOBJS; ?>"></script>
+<script src="<?php echo $_BOOTSTRAPWYSIHMTL5JS; ?>"></script>
+<script src="<?php echo $_ICHECKJS; ?>"></script>
+<script src="_required/js/app.js"></script>
+<script src="_required/js/dashboard.js"></script>
+
