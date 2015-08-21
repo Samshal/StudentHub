f0dbd8ea2c191411cf8e2296d2d1715ae36560d2
Samuel Adeshina <samueladeshina73@gmail.com>
2015-08-21 14:31:05 +0100

    Shared Resources/Scripts

---
+++ View/Shared/index.php
@@ -0,0 +1,10 @@
+<?php
+	require_once("..\..\__Alloc\__Alloc.php");
+	$_OUT = "..\\";
+	$_BOOTSTRAP = $_OUT. __EXTERNAL_FOLDER."\Bootstrap\\";
+	$_JQUERY = $_OUT. __EXTERNAL_FOLDER."\JQuery\jQuery.min.js";
+	$_FA = $_OUT.__EXTERNAL_FOLDER."\\font-awesome\\font-awesome.min.css";
+	$_BOOTSTRAPCSS = $_BOOTSTRAP."css\bootstrap.min.css";
+	$_BOOTSTRAPJS = $_BOOTSTRAP."js\bootstrap.min.js";
+	define("_APPNAME_", "StudentHub");
+?>
