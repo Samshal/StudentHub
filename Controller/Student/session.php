2087839f422a7d256e3b53a9a51fa7f178280bea
Samuel Adeshina <samueladeshina73@gmail.com>
2015-08-25 01:58:18 +0100

    Added more arrays that contains information about the details of the currently logged in user

--- Controller/Student/session.php
+++ Controller/Student/session.php
@@ -8,14 +8,34 @@
 
 	$model = new Model();
 
-	$_name = $model->Select("[Students].[Info]", array("FirstName", "MiddleName", "LastName"), array("StudentID"=>$_SESSION["USERID"]));
+	$_name = $model->Select("[Students].[Info]", array("*"), array("StudentID"=>$_SESSION["USERID"]));
+	$_profile = $model->Select("[Students].[Profile]", array("*"), array("Student"=>$_SESSION["USERID"]));
+	$courseid = $model->Select("[Students].[Course]", array("Course"), array("Student"=>$_SESSION["USERID"]))[0]["Course"];
+	$semesterid = $model->Select("[Students].[Semester]", array("Semester"), array("Student"=>$_SESSION["USERID"]))[0]["Semester"];
+	$_semester = $model->Select("[General].[Semester]", array("*"), array("SemesterID"=>$semesterid));
+	$_course = $model->Select("[General].[Course]", array("*"), array("CourseID"=>$courseid));
 	
 	DEFINE("FULLNAME", $_name[0]["FirstName"]." ".$_name[0]["MiddleName"]." ".$_name[0]["LastName"]);
 	DEFINE("FIRSTNAME", $_name[0]["FirstName"]);
 	DEFINE("LASTNAME", $_name[0]["LastName"]);
 	DEFINE("MIDDLENAME", $_name[0]["MiddleName"]);
-
-
+	if (isset($_profile[0]["Picture"]))
+	{
+		DEFINE("IMAGESRC", $_profile[0]["Picture"]);
+	}
+	if (isset($_profile[0]["TagLine"]))
+	{ 
+		DEFINE("TAGLINE", $_profile[0]["TagLine"]);
+	}
+	if (isset($_course[0]["CourseName"]))
+	{
+		DEFINE("COURSENAME", $_course[0]["CourseName"]);
+		DEFINE("COURSEDURATION", $_course[0]["CourseDuration"]);
+	}
+	if (isset($_semester[0]["SemesterName"]))
+	{
+		DEFINE("SEMESTERNAME", $_semester[0]["SemesterName"]);
+	}
 
 	$model = null;
 ?>
