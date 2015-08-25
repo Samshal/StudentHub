3778587b79e99e2378e15c14ede595e894045eae
Samuel Adeshina <samueladeshina73@gmail.com>
2015-08-25 02:07:04 +0100

    Added user image

--- View/Student/_headers/page-parts/user-panel.php
+++ View/Student/_headers/page-parts/user-panel.php
@@ -1,6 +1,6 @@
 <div class="user-panel">
     <div class="pull-left image">
-        <img src="img/avatar3.png" class="img-circle" alt="User Image" />
+        <img src="<?php echo "../../__TempFiles/__Uploads/Students/".IMAGESRC;?>" class="img-circle" alt="User Image" />
     </div>
     <div class="pull-left info">
         <p>Hello, <?php echo FIRSTNAME; ?></p>
