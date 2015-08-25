e45fd3b57ae957cab83b96c70b4c6f12b1b4cab3
Samuel Adeshina <samueladeshina73@gmail.com>
2015-08-25 02:10:51 +0100

    Updated the rules for redirection

--- View/Student/index.php
+++ View/Student/index.php
@@ -5,6 +5,20 @@
 	require_once("_headers/header.php");
 	require_once("_headers/app-menu.php");
 	require_once("_call.php");
-	display("dashboard");
+	if (isset($_GET["page"]))
+	{
+		if (file_exists("page-parts/".$_GET["page"].".php"))
+		{
+			display($_GET["page"]);
+		}
+		else
+		{
+			display("404");
+		}
+	}
+	else
+	{
+		display("dashboard");
+	}
 	require_once("_footer/footer.php");
 ?>
